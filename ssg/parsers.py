8d1e76b2e42a7c8502a1b89c8c05418d2e902ba2
Muhammad Usama <usamaa9>
2022-05-01 05:33:07 +0100

    bug

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -54,7 +54,7 @@ class MarkdownParser(Parser):
 
 class ReStructuredTextParser(Parser):
 
-    extensions = [".rst"]
+    extensions = [".rstd"]
 
     def parse(self, path: Path, source: Path, dest: Path):
         content = Content.load(self.read(path))
