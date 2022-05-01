659bfcffa8ba3f6e94a34edab06df1e66b1c55a0
Muhammad Usama <usamaa9>
2022-05-01 04:13:42 +0100

    fixed typo

--- ssg/site.py
+++ ssg/site.py
@@ -12,6 +12,6 @@ class Site:
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
-        for path in self.source.glob("*"):
+        for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
