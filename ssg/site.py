e3f1607aaede052cd135424c9b0b8e8889fa9b89
Muhammad Usama <usamaa9>
2022-05-01 04:09:58 +0100

    added site class and ssg

---
+++ ssg/site.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path: Path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.glob("*"):
+            if path.is_dir():
+                self.create_dir(path)
